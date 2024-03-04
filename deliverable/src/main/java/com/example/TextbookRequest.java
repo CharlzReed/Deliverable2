00cd253ba2d5efb372e0af447981f2fe76361eb9
Charles <77180116+CharlzReed@users.noreply.github.com>
2024-03-04 17:55:52 -0500

    Update TextbookRequest.java

--- deliverable/src/main/java/com/example/TextbookRequest.java
+++ deliverable/src/main/java/com/example/TextbookRequest.java
@@ -2,4 +2,12 @@ package com.example;
 
 public class TextbookRequest {
 
+    public int bookID;
+    public int userID;
+    public int requestID;
+
+    public void notifyUser(int bookID, int requestPriority) {
+        // TODO
+    }
+
 }
