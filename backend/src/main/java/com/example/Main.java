cf8598e2e82fa4cc9bcad6516db506b91f64fa06
Moses <91290515+mosesluna@users.noreply.github.com>
2024-04-08 21:34:15 -0400

    Update Main.java
    commented out main because this class does not matter

--- backend/src/main/java/com/example/Main.java
+++ backend/src/main/java/com/example/Main.java
@@ -3,34 +3,34 @@ package com.example;
 public class Main {
 
     // This is for testing purposes. In order to run the actual GUI go to frontend/src/main/java/EECS3311Deliverable2/frontend/Launcher.java and click run.
-    public static void main(String[] args) {
-        Library library = Library.getInstance();
+    //public static void main(String[] args) {
+        //Library library = Library.getInstance();
 
         // Assuming CSVReader.readALL() properly initializes the library with users,
         // items, etc.
-        CSVReader.readALL();
+        //CSVReader.readALL();
 
         // Simulate adding a new user
-        library.registerNewUser(
-                library.generateRandomNumber(),
-                "John Doe",
-                "john.doe@example.com",
-                "securePassword123",
-                UserType.STUDENT,
-                100.0);
+        //library.registerNewUser(
+                //library.generateRandomNumber(),
+                //"John Doe",
+                //"john.doe@example.com",
+                //"securePassword123",
+                //UserType.STUDENT,
+                //100.0);
 
-        Item newItem = new Item(1, "The Example Book", ItemType.BOOK, LocationType.NORTH_SHELF, 10.99,
-                StatusType.ACTIVE);
-        library.addItem(newItem);
-        newItem.applyDiscountStrategy(new FreeDiscountStrategy());
-        System.out.println("Cost after discount: " + newItem.getCostAfterDiscount());
+        //Item newItem = new Item(1, "The Example Book", ItemType.BOOK, LocationType.NORTH_SHELF, 10.99,
+                //StatusType.ACTIVE);
+        //library.addItem(newItem);
+        //newItem.applyDiscountStrategy(new FreeDiscountStrategy());
+        //System.out.println("Cost after discount: " + newItem.getCostAfterDiscount());
 
         // Simulate creating and adding new items
 
         // Retrieve a user and have them rent the new book
-        User user = library.searchUsers("John Doe", 1).get(0);
-        user.rentItem(newItem);
+        //User user = library.searchUsers("John Doe", 1).get(0);
+        //user.rentItem(newItem);
 
-        CSVReader.writeALL();
-    }
+        //CSVReader.writeALL();
+    //}
 }
